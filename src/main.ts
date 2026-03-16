f238a9919c86e902f94a0f1e3cbd96570165ba23
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-03-16 11:11:22 -0300

    docs: enable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Email Delivery API')
+      .setDescription(
+        'A lightweight microservice built with NestJS for sending personal emails. It provides a simple API to trigger email delivery for personal projects.',
+      )
+      .setVersion('1.0')
+      .build(),
+  );
+  const theme = new SwaggerTheme();
+  SwaggerModule.setup('/api', app, document, {
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
