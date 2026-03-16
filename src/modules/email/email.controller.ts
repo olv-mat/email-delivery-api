abc1a9d1f98d9626b1d2f1a9792cc321a87f46a6
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-03-16 11:30:38 -0300

    docs: document send email endpoint

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiInternalServerErrorResponse, ApiOperation } from '@nestjs/swagger';
 import { DefaultResponseDto } from './dtos/default-response.dto';
 import { SendEmailDto } from './dtos/send-email.dto';
 import { EmailService } from './email.service';
@@ -8,6 +9,16 @@ export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Send a personal email' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        statusCode: 500,
+        message: 'Internal server error',
+      },
+    },
+  })
   public send(@Body() dto: SendEmailDto): Promise<DefaultResponseDto> {
     return this.emailService.send(dto);
   }
