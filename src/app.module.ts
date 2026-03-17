e314d0bf03f2d6a53180027a8f2c9e39f20adf73
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-03-17 13:27:54 -0300

    chore: add throttler for rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { EmailModule } from './modules/email/email.module';
 
+// npm i @nestjs/throttler
+
 @Module({
-  imports: [EmailModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    EmailModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 1,
+        },
+      ],
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
