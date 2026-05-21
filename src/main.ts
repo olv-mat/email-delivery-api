01c36042faed8e30cd17e3a5f850d8397bc2fce5
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-05-21 12:25:34 -0300

    chore: change enums folder to common module

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { Environments } from './common/enums/environments.enum';
 import { setupSwagger } from './common/swagger/setup.swagger';
-import { Environments } from './modules/enums/environments.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
